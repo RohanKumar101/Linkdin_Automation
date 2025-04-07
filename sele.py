fbb8d6285f819d88f8f28df4d4cade0931c4de45
RohanKumar101 <rohitgoraimp@gmail.com>
2025-04-07 20:33:00 +0530

    add profile search

--- sele.py
+++ sele.py
@@ -4,7 +4,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+from bs4 import BeautifulSoup
 import os
+import csv
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -31,9 +33,40 @@ login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XP
 login_button.click()
 
 
+
+
 time.sleep(5)
 
-print("Login Successful!")
+profile_urls = [
+    'https://www.linkedin.com/in/princeyadav05',
+    'https://www.linkedin.com/in/nikhilsharma05',
+]
+
+with open('profiles.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Name', 'Headline'])
+
+
+    for profile_url in profile_urls:
+        driver.get(profile_url)
+        time.sleep(5)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+        name_tag = soup.select_one("h1.mnqQoMADCIfVsNsuwkCAAQYLQjpGSdgnKxai.inline.t-24.v-align-middle.break-words")
+        name = name_tag.text.strip() if name_tag else "N/A"
+
+
+        headline_tag = soup.select_one("div.text-body-medium.break-words")
+        headline = headline_tag.text.strip() if headline_tag else "N/A"
+
+
+   
+    
+      
+        print("Name:", name)
+        print("Headline:", headline)
+     
+        writer.writerow([name, headline,])
 
 
 driver.quit()
